d3da7ef86939a2dc174852f7a85e80e9a667cd01
anthonytobiesq <104419051+anthonytobiesq@users.noreply.github.com>
2022-05-28 09:20:15 +0200

    added imageView recent changes 2805

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -40,26 +40,14 @@ class Dice(private val numSides: Int) {
         return (1..numSides).random()
     }
 }
+
+
+
 }
 
-private fun rollDice() {
-    val dice = Dice(6)
-    val diceRoll = dice.roll()
 
-    val diceImage: ImageView = findViewById(R.id.imageView)
 
-    when (diceRoll) {
-        1 -> diceImage.setImageResource(R.drawable.dice_1)
-        2 -> diceImage.setImageResource(R.drawable.dice_2)
-        3 -> diceImage.setImageResource(R.drawable.dice_3)
-        4 -> diceImage.setImageResource(R.drawable.dice_4)
-        5 -> diceImage.setImageResource(R.drawable.dice_5)
-        6 -> diceImage.setImageResource(R.drawable.dice_6)
-    }
-}
 
-class Dice(val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
-}
+
+
+
